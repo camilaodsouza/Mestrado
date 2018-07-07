0e2ac95dc81c897c62ab49718fdf64e56db0282e
dlmacedo <dlm@cin.ufpe.br>
2018-07-07 13:52:44 -0300

    Working fine...

--- train.sh
+++ train.sh
@@ -2,23 +2,23 @@
 
 export run=train
 
-export dataset=cifar10
-for model in alexnet_ squeezenet_ squeezemobnet_ mobilenet_ mobilenetv2_
-do
-  export exps="baseline"
-  python $run.py -d $dataset -lm $model -exps $exps | tee artifacts/$run.$dataset.$model.$exps.txt
-done
+#export dataset=cifar10
+#for model in alexnet_ squeezenet_ squeezemobnet_ mobilenet_ mobilenetv2_
+#do
+#  export exps="baseline"
+#  python $run.py -d $dataset -lm $model -exps $exps | tee artifacts/$run.$dataset.$model.$exps.txt
+#done
 
-export dataset=cifar100
-for model in alexnet_ squeezenet_ squeezemobnet_ mobilenet_ mobilenetv2_
-do
-  export exps="baseline"
-  python $run.py -d $dataset -lm $model -exps $exps | tee artifacts/$run.$dataset.$model.$exps.txt
-done
+#export dataset=cifar100
+#for model in alexnet_ squeezenet_ squeezemobnet_ mobilenet_ mobilenetv2_
+#do
+#  export exps="baseline"
+#  python $run.py -d $dataset -lm $model -exps $exps | tee artifacts/$run.$dataset.$model.$exps.txt
+#done
 
 export dataset=imagenet2012
 for model in alexnet squeezenet1_1 squeezemobnet1_1 mobilenet mobilenetv2
 do
   export exps="baseline"
-  python $run.py -d $dataset -lm $model -exps $exps | tee artifacts/$run.$dataset.$model.$exps.txt
+  python $run.py -d $dataset -lm $model -exps $exps -x 1 | tee artifacts/$run.$dataset.$model.$exps.txt
 done
